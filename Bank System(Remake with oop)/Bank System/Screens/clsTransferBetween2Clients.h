37c9f0db0892bf0f88f561e79986dab75970d4ba
osama mohamoud <wwwhdosama@gmail.com>
2025-12-21 00:55:38 +0200

    Auto commit: 2025-12-21 00:55:37

--- Bank System(Remake with oop)/Bank System/Screens/clsTransferBetween2Clients.h
+++ Bank System(Remake with oop)/Bank System/Screens/clsTransferBetween2Clients.h
@@ -117,9 +117,11 @@ public :
 			clsBankClient ClientWhoWillReceive = _CheckAccountBeforeContinue(); // check if the second account is existing 
 
 			if (ClientWhoWillReceive.GetAccountNumber() == ClientWhoWillSend.GetAccountNumber()) {
-				cout << "\n\n\t\t\t\t" << colorText("\nYOU CANN'T TRANSFER TO YOURSELF!!!!", "red")<<"\n";
+				system("cls");
+				cout <<  colorText(" \n\n\t\t\t\t\aYOU CANN'T TRANSFER TO YOURSELF!!!!", "red")<<"\n";
 				// undo --> return;
-				clsManageClientBalance::Deposit(ClientWhoWillSend,amount);  // return the amount that he withdrawed 
+				clsManageClientBalance::Deposit(ClientWhoWillSend,amount);  // return the amount that he withdrawed
+				return; 
 			}
 
 			SaveStatus = clsManageClientBalance::Deposit(ClientWhoWillReceive, amount);
