1f8f5fdd5b69c88a4093312b649403433ea5fa79
osama mohamoud <wwwhdosama@gmail.com>
2025-12-20 15:01:47 +0200

    Auto commit: 2025-12-20 15:01:47

--- Bank System(Remake with oop)/Bank System/Screens/clsFindClientScreen.h
+++ Bank System(Remake with oop)/Bank System/Screens/clsFindClientScreen.h
@@ -14,41 +14,6 @@ public:
 		// Feature : make user attempt for 3 times if he didn't find the account ask him if he want to continue or not 
 	
 		short Trials =0;
-		bool Exit = false; 
-	               //  check if client is existing if it exits then return object ///
-		do {
-			Trials++;
-			system("cls");
-			cout << "\n\n\n\n";
-			clsScreen::_PrintMenuOption(colorText("            Find Client Screen", "yellow"));
-
-			if (Trials > 4) 
-			{
-			
-				std::cout << colorText("\n\t\t\t\t\t\aYou Tried For " + to_string(Trials-1) + " Times To Find The Account !\n\t\t\t\t\tExit To Main Menu Or Try Again ? [y], [n] : ", "red");
-				if (clsInputAndValidation::Confirm("")) {
-					Exit = true;
-					break;
-				}
-				else {
-					Trials = 0;
-					Exit = false;
-					FindClientByAccountNumber();
-				}
-
-			}
-
-
-
-
-			AccountNumber = clsInputAndValidation::read_string("\n\t\t\t\t\tEnter Account Number : ");
-
-		} while ( !(clsBankClient::ReturnCurrentClient_if_ItExists(AccountNumber, Client) ) && (Exit == false));
-
-
-	  // if exists
-		if(!Exit) clsUtilPrintClientData::PrintClientData(Client);
-
 
 		while (true) 
 		{
@@ -61,18 +26,24 @@ public:
 
 			if (Trials > 4)
 			{
-				std::cout << colorText("\n\t\t\t\t\t\aYou Tried For " + to_string(Trials - 1) + " Times To Find The Account !\n\t\t\t\t\tExit To Main Menu[y] Or Try Again[n] ? [y], [n] : ", "red");
-				if (clsInputAndValidation::Confirm("")) {
-					break; // exit the loop and will call return to the main menu 
+				std::cout << colorText("\n\t\t\t\t\t\aYou Tried For " + to_string(Trials - 1) + " Times To Find The Account !\n\t\t\t\t\tExit To Main Menu[y] Or Try Again[n] ? : ", "red");
+				if (clsInputAndValidation::Confirm("")) { // if user entered 'Y' then it will returns true 'confirm()'
+					break ; // Back to main menu 
 				}
 				else {
 					Trials = 0;
-					Exit = false;
-					FindClientByAccountNumber();
+					continue; // exit the loop and will call return to the main menu 
 				}
 
 			}
 
+
+			AccountNumber = clsInputAndValidation::read_string("\n\t\t\t\t\tEnter Account Number : ");
+
+			if ( clsBankClient::ReturnCurrentClient_if_ItExists(AccountNumber, Client) ) { // print client data then break
+				clsUtilPrintClientData::PrintClientData(Client);
+				break; 
+			}
 
 
 
