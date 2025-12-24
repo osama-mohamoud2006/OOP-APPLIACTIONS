34f830bd971a8bba636f8461a4fe7c833b9b2a15
osama mohamoud <wwwhdosama@gmail.com>
2025-12-25 00:37:19 +0200

    Auto commit: 2025-12-25 00:37:19

--- Bank System(Remake with oop)/Bank System/Main.cpp
+++ Bank System(Remake with oop)/Bank System/Main.cpp
@@ -8,19 +8,43 @@ using namespace std;
 
 ///  ctrl + m+o
 
+void PrintUserInfo( clsUser & user ) 
+{
+	cout << "the user name is : " << user.GetUserName() << endl; 
+	cout << "the Full name is :  " << user.GetFullName() << endl;
+	cout << "the Email is : " << user.GetEmail() << endl;
+	cout << "the Phone is : " << user.GetPhone() << endl;
+	cout << "the Permissions is : " << user.GetPermissions() << endl;
+	cout << "----------------------------------------" << endl;
+	cout << "the Password is : " << user.GetPassword() << endl;
+
+}
+
+void FindUser() 
+{
+	string UserName = "";
+	clsUser user = clsUser::ReturnEmptyObjForInitializingUser();
+	UserName = clsInputAndValidation::read_string("\n\t\t\t\tEnter Username : ");
+
+	while (!( clsUser::FindUserAndReturnObj_If_exist(UserName,user) )  )  // if it isn't existing 
+	{
+		cout << "\n\t\t\t\t\aThe Username " << UserName << " isn't existing !\n";
+		UserName = clsInputAndValidation::read_string("\n\t\t\t\tEnter Another Username : ");
+	}
+
+	cout<< "the User is found successfully \n";
+	PrintUserInfo(user);
+
+}
+
+
+
+
 int main() {
 
 
 ///clsMainMenuScreen::_ShowMainMenuScreen();
 
-	//clsUser User1 = clsUser::ReturnEmptyObjForInitializingUser();
-	//string line = "username#//#password#//#0#//#first name#//# last name#//#email#//#phone";
-	//User1._AddLineToFile(line); 
-	//User1._ConvertLineToObject(line); 
-
-	clsUser user = clsUser::ReturnEmptyObjForInitializingUser();
-		 clsUser::FindUserAndReturnObj_If_exist("Admin", user);
-		cout << user.GetUserName() << endl; 
-		 return 0;
+	FindUser();
 
 }
