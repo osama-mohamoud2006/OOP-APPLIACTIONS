a6c19c78e701d6fe38188e0dd714d34d7dde24cb
osama mohamoud <wwwhdosama@gmail.com>
2025-12-27 18:38:55 +0200

    Auto commit: 2025-12-27 18:38:54

--- Bank System(Remake with oop)/Bank System/enc_temp_folder/6f434963938859944f4ce64dc11093/clsAddUsers.h
+++ Bank System(Remake with oop)/Bank System/enc_temp_folder/6f434963938859944f4ce64dc11093/clsAddUsers.h
@@ -39,7 +39,7 @@ private:
         cout << setw(leftPadding) << "" << "    Email      : " << user.GetEmail() << "\n";
         cout << setw(leftPadding) << "" << "    Phone      : " << user.GetPhone() << "\n\n";
 
-        cout << setw(leftPadding) << "" << colorText("  USER DETAILS", "yellow") << "\n";
+        cout << setw(leftPadding) << "" << colorText("  ACCOUNT DETAILS", "yellow") << "\n";
         cout << setw(leftPadding) << "" << "  ----------------------------------------------\n";
         cout << setw(leftPadding) << "" << "    Permissions: " << colorText(to_string(user.GetPermissions()), "magenta") << "\n";
         cout << setw(leftPadding) << "" << "    Password   : " << colorText(user.GetPassword(), "red") << "\n\n";
@@ -50,7 +50,7 @@ private:
    static void _FillNewUser(clsUser& User) {
         string Data = "";
         cout << "\n\n";
-        clsScreen::_PrintMenuOption(colorText("           Adding New User ", "green")); // will replace the clsScreen print option
+        clsScreen::_PrintMenuOption(colorText("        Adding New User ", "green")); // will replace the clsScreen print option
 
         Data = clsInputAndValidation::read_full_line("\n\t\t\t\t\tEnter password: ");
         User.SetPassword(Data);
@@ -87,8 +87,8 @@ public:
         while (clsUser::FindUserAndReturnObj_If_exist(Username, user))  // if the user exists 
         {
             screen_color(red);
-            cout << "\n\n\t\t\t\t\tThe User " << Username << " Exists!\a\n";
-            Username = clsInputAndValidation::read_string("\n\t\t\t\t\tEnter Username: ");
+            cout << "\n\n\t\t\t\tThe User " << Username << " Exists!\a\n";
+            Username = clsInputAndValidation::read_string("\n\t\t\t\tEnter Username: ");
         }
 
         system("color 0F"); // rest color 
@@ -105,24 +105,21 @@ public:
 
         case clsUser::enSave::enSavedSuccessfully:
         {
-            system("cls");
-            cout << colorText("\n\t\t\t\t\tUser: " + Username + " Added Successfully ! \n", "green");
+            cout << colorText("User: " + Username + " Added Successfully ! \n", "green");
             _PrintUserInfo(user);
             break;
         };
 
         case clsUser::enSave::enUsernameExists: 
         {
-            system("cls");
-            string message = "\n\t\t\t\t\tUsername " + Username + " Exists " + "Failed To Save";
+            string message = "\n\t\t\t\tUsername " + Username + " Exists " + "Failed To Save";
             cout << colorText(message, "red") << endl;
             break;
         };
 
         case clsUser::enSave::enFailedOrEmptyToSave: 
         {
-            system("cls");
-            cout << colorText("\n\t\t\t\t\tFailed To Write On File!\n\a", "red");
+            cout << colorText("\n\t\t\t\tFailed To Write On File!\n\a", "red");
             break;
         };
 
