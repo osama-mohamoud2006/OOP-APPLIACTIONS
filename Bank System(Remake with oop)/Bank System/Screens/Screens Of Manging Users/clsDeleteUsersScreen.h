51483680d738e3e4d1dc622fd0f7fa53d675ac39
osama mohamoud <wwwhdosama@gmail.com>
2025-12-27 19:58:53 +0200

    Auto commit: 2025-12-27 19:58:53

--- Bank System(Remake with oop)/Bank System/Screens/Screens Of Manging Users/clsDeleteUsersScreen.h
+++ Bank System(Remake with oop)/Bank System/Screens/Screens Of Manging Users/clsDeleteUsersScreen.h
@@ -9,18 +9,18 @@ public :
 
    static void DeleteUser()
     {
-       clsScreen::_PrintMenuOption(colorText("      Delete Users Screen","red"));
+       clsScreen::_PrintMenuOption(colorText("         Delete Users Screen","red"));
 
         clsUser user = clsUser::ReturnEmptyObjForInitializingUser();
         string Username = "";
-        Username = clsInputAndValidation::read_string("\n\t\t\t\tEnter Username: ");
+        Username = clsInputAndValidation::read_string("\n\t\t\t\t\tEnter Username: ");
 
-        // wonot allow to delete the "admin" as it is the main user and he controls every thing
-        while (!clsUser::FindUserAndReturnObj_If_exist(Username, user) && Username == "admin")  // if the user exists 
+        // won't allow to delete the "admin" as it is the main user and he controls every thing
+        while (!clsUser::FindUserAndReturnObj_If_exist(Username, user) || Username == "admin")  // if the user exists 
         {
             screen_color(red);
-            cout << "\n\n\t\t\t\tThe User " << Username << " isn't Exists!\a\n";
-            Username = clsInputAndValidation::read_string("\n\t\t\t\tEnter Username: ");
+            cout << "\n\n\t\t\t\t\tThe User " << Username << " isn't Exists!\a\n";
+            Username = clsInputAndValidation::read_string("\n\t\t\t\t\tEnter Username: ");
         }
 
         screen_color(red_on_black);
@@ -31,7 +31,7 @@ public :
             clsUtilPrintUserDetails::PrintUserInfo(user);
         }
         else {
-            cout <<colorText( "\n\n\n\NO change!\n","pink");
+            cout <<colorText( "\n\n\n\t\t\t\t\tNO change!\n","pink");
         }
 
     }
