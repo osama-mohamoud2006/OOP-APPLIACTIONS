cd7b20ee987c92a5ceef11f088b50cc40b4e5dde
osama mohamoud <wwwhdosama@gmail.com>
2025-12-31 00:57:23 +0200

    Auto commit: 2025-12-31 00:57:23

--- Bank System(Remake with oop)/Bank System/Screens/clsTransferBetween2Clients.h
+++ Bank System(Remake with oop)/Bank System/Screens/clsTransferBetween2Clients.h
@@ -107,14 +107,18 @@ public :
 
 		}
 
-         clsBankClient::enSaveMode SaveStatus;
+
+         clsBankClient::enSaveMode SaveStatus = clsBankClient::enSaveMode::FailedOrEmptyObj; // initialize 
 		 clsBankClient ClientWhoWillReceive = clsBankClient::EmptyObjForInitializing();
+		 bool Confirmed = false; 
 
 		// ClientWhoWillSend ---> withdraw from him
-		if (clsInputAndValidation::Confirm(colorText("\n\t\t\t\tAre You Sure [y],[n]: ","red")))
+		if (  (Confirmed= clsInputAndValidation::Confirm(colorText("\n\t\t\t\tAre You Sure [y],[n]: ","red"))  )   )
 		{
 			
 			system("cls");
+			clsBankClient::enSaveMode  WasWithDrawDone = clsBankClient::enSaveMode::FailedOrEmptyObj; // check if the withdraw from who will send done or not 
+
 			clsScreen::_PrintMenuOption(colorText("     The Client Who Will Receive", "cyan"));
 			ClientWhoWillReceive = _CheckAccountBeforeContinue(); // check if the second account is existing  -- > if found it who will receive will have the  object
 
@@ -124,30 +128,47 @@ public :
 				system("cls");
 				cout << colorText(" \n\n\t\t\t\t\aYOU CANN'T TRANSFER TO YOURSELF!!!!", "red") << "\n";
 				// undo --> return;
-				//clsManageClientBalance::Deposit(ClientWhoWillSend, amount);  // return the amount that he withdrawal
 				return;
 			}
 
-			// check if the both operation done 
-			else if (clsManageClientBalance::WithDraw(ClientWhoWillSend, amount) == clsBankClient::enSaveMode::SuccessedToSave
+			// check if the withdraw done
+			if (clsManageClientBalance::WithDraw(ClientWhoWillSend, amount) == clsBankClient::enSaveMode::SuccessedToSave )
+			{
+				WasWithDrawDone = clsBankClient::enSaveMode::SuccessedToSave;
+			}
+			else {   // if not 
+				cout << "\n\n\a\t\t\t\t\tWITHDRAW FAILED !\n";
+				clsManageClientBalance::Deposit(ClientWhoWillSend, amount);  // return the amount that he withdrawal
+				return; 
+			}
+
+			// if withdraw done 
+			 if (WasWithDrawDone == clsBankClient::enSaveMode::SuccessedToSave
 				&& clsManageClientBalance::Deposit(ClientWhoWillReceive, amount) == clsBankClient::enSaveMode::SuccessedToSave)
 			{
 				SaveStatus = clsBankClient::enSaveMode::SuccessedToSave;
 			}
 			
+
 		}
 
-		if (clsBankClient::enSaveMode::SuccessedToSave == SaveStatus)
-		{
-			system("cls");
-			screen_color(black);
-			_PrintSuccess(ClientWhoWillSend, ClientWhoWillReceive, amount);
+		if (!Confirmed) cout << colorText("\n\n\t\t\t\t\tNO CHANGE!\n","pink"); // if not confirmed by y 
+		else {
+
+			if (clsBankClient::enSaveMode::SuccessedToSave == SaveStatus)
+			{
+				system("cls");
+				screen_color(black);
+				_PrintSuccess(ClientWhoWillSend, ClientWhoWillReceive, amount);
+			}
+
+			else
+			{
+				_PrintTransferFailed(ClientWhoWillSend, ClientWhoWillReceive, amount, ClientWhoWillSend.GetBalance());
+			}
+
 		}
 
-		else
-		{
-			_PrintTransferFailed(ClientWhoWillSend, ClientWhoWillReceive, amount, ClientWhoWillSend.GetBalance());
-		}
 
 	};
 
