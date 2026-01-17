4c240f4fea8880bb85592f7d45c096490c65fe8d
osama mohamoud <wwwhdosama@gmail.com>
2026-01-17 02:43:10 +0200

    Auto commit: 2026-01-17 02:43:10

--- Bank System(Remake with oop)/Bank System/CurrencyExchange/Screens/clsCurrencyCalc.h
+++ Bank System(Remake with oop)/Bank System/CurrencyExchange/Screens/clsCurrencyCalc.h
@@ -5,7 +5,7 @@ class clsCurrencyCalc : protected clsScreen
 {
 
 private :
-	static  clsCurrencyExchange CurrencyToOrFrom(const string &Message)
+	static  clsCurrencyExchange _CurrencyToOrFrom(const string &Message)
 	{
 		string CurrencyCode = clsInputAndValidation::read_string("\n\t\t\tEnter Currency Code "+ Message+" : ");
 		//short Trials = 5;
@@ -23,7 +23,7 @@ private :
 		return C;
 	}
 
-	static void PrintFromAnyCurrencyToUSD_Details(clsCurrencyExchange &  c ,double Amount , double  AnyCurrencyToUSDRes )
+	static void _PrintFromAnyCurrencyToUSD_Details(clsCurrencyExchange &  c ,double Amount , double  AnyCurrencyToUSDRes )
 	{
 
 		int terminalWidth = 120;
@@ -51,8 +51,7 @@ private :
 		cout << setw(leftPadding) << "" << colorText("================================================", "cyan") << "\n\n";
 	}
 
-
-	static void PrintFromUSDToAnyCurrencyDetails(double Amount, double TheConvertedAmount,clsCurrencyExchange&FromCurrencyDetails, clsCurrencyExchange& ToCurrencyDetails)
+	static void _PrintFromUSDToAnyCurrencyDetails(double Amount, double TheConvertedAmount,clsCurrencyExchange&FromCurrencyDetails, clsCurrencyExchange& ToCurrencyDetails)
 	{
 		int terminalWidth = 120;
 		int contentWidth = 48;
@@ -81,8 +80,8 @@ private :
 		clsScreen::_PrintMenuOption(colorText("       Currency Calculator Screen","pink"));
 
 		
-		clsCurrencyExchange TheCurrencyFrom = CurrencyToOrFrom("Of The Currency You Want To Convert From"); // From
-		clsCurrencyExchange TheCurrencyTo= CurrencyToOrFrom("Of The Currency You Want To Convert To"); // To
+		clsCurrencyExchange TheCurrencyFrom = _CurrencyToOrFrom("Of The Currency You Want To Convert From"); // From
+		clsCurrencyExchange TheCurrencyTo= _CurrencyToOrFrom("Of The Currency You Want To Convert To"); // To
 
 		while (TheCurrencyFrom.GetCurrencyCode() == TheCurrencyTo.GetCurrencyCode())
 		{
@@ -93,12 +92,12 @@ private :
 		double Amount = clsInputAndValidation::enter_postive_number(colorText("\n\n\t\t\tEnter The Amount You Want To Convert To "+ TheCurrencyTo.GetCurrencyCode()+" : ","green"));
 		double ConversationToUSD = TheCurrencyFrom.AnyCurrencyToUSD(Amount);
 
-		if (TheCurrencyTo.GetCurrencyCode() == "USD") { system("cls");  PrintFromAnyCurrencyToUSD_Details(TheCurrencyFrom, Amount, ConversationToUSD); }
+		if (TheCurrencyTo.GetCurrencyCode() == "USD") { system("cls");  _PrintFromAnyCurrencyToUSD_Details(TheCurrencyFrom, Amount, ConversationToUSD); }
 		else
 		{
 			system("cls");
-			//PrintFromAnyCurrencyToUSD_Details(TheCurrencyFrom, Amount, ConversationToUSD);
-			PrintFromUSDToAnyCurrencyDetails(Amount, TheCurrencyTo.FromUSDToAnyCurrency(ConversationToUSD), TheCurrencyFrom, TheCurrencyTo);
+			//_PrintFromAnyCurrencyToUSD_Details(TheCurrencyFrom, Amount, ConversationToUSD);
+			_PrintFromUSDToAnyCurrencyDetails(Amount, TheCurrencyTo.FromUSDToAnyCurrency(ConversationToUSD), TheCurrencyFrom, TheCurrencyTo);
 	
 		}
 		
