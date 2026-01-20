5450b575cf9d298cdaf08429eaead158485499b8
osama mohamoud <wwwhdosama@gmail.com>
2026-01-20 17:42:17 +0200

    Auto commit: 2026-01-20 17:42:16

--- Input&Validation Library/main.cpp
+++ Input&Validation Library/main.cpp
@@ -15,8 +15,9 @@ int main() {
 
   // cout<<  clsInputAndValidation::IsNumberBetween(4,1,10)<<endl;;
 cout<<"enter num: ";
- double x = clsInputAndValidation::enter_number_from_to(1,10);
+ double x = clsInputAndValidation::clsReadNum<int>::enter_number_from_to(1,10);
 
+ string str = clsInputAndValidation::read_string("enter any thing: "); 
   //   cout<<  clsInputAndValidation::IsDateBetween(clsDate(31,12,2025), clsDate(1,1,2022), clsDate(31,12,2025) )<<endl;  // ✅ true
   // cout<< clsInputAndValidation::IsDateBetween(clsDate(15,6,2024), clsDate(31,12,2024),clsDate( 1,1,2024) )  <<endl ;  // ✅ true
   //
